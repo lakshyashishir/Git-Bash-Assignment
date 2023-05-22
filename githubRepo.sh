21bf805b8ab3a4633aadc8b62e8fba454e02760a
lakshyashishir <checkinglakshya@gmail.com>
2023-05-23 00:42:59 +0530

    add enviroment variable and mulitple file

--- githubRepo.sh
+++ githubRepo.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+#!/usr/bin/env sh
+# prog.sh
+
+source ./config.txt
 
 echo "Create GitHub Repo"
+read -p "Do you want to use environment variables for GitHub configuration [y/n]: " envCheck
 
-read -p "Enter your GitHub username: " ghUsername
-read -p "Enter your GitHub personal access token: " pat
+if [[ "$envCheck" == "y" || "$envCheck" == "Y" ]]; then
+    ghUsername="$Github_Username"
+    pat="$ghkey"
+else
+    read -p "Enter your GitHub username: " ghUsername
+    read -p "Enter your GitHub personal access token: " pat
+fi
+
+fileAdded=false
+
+patCheck=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token $pat" https://api.github.com/user)
+
+while ! [[ "$patCheck" -eq 200 ]]; do
+    echo "Personal Access Token is invalid or unauthorized."
+    read -p "Enter your GitHub personal access token: " pat
+    patCheck=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token $pat" https://api.github.com/user)
+done
+
 read -p "Enter a repository name: " repoName
 read -p "Enter repository description: " repoDescription
 read -p "Do you want the repository to be public [y/n]: " publicBool
@@ -26,27 +47,43 @@ curl -u "$ghUsername:$pat" -X POST \
         "private": '$repoType'
     }'
 
-read -p "Do you want to add a README.md in this repo [y/n]: " readmeBool
+read -p "Do you want to add a README.md to this repo [y/n]: " readmeBool
+read -p "Do you want to create any file for this repo [y/n]: " fileBool
 
-read -p "Commit message: " commitMSG
+if [[ "$readmeBool" == "y" || "$readmeBool" == "Y" || "$fileBool" == "y" || "$fileBool" == "Y" ]]; then
+    fileAdded=true
+    mkdir -p "$repoName"
+    cd "$repoName"
+    git init
 
-mkdir -p "$repoName"
-cd "$repoName"
-git init
+    if [[ "$readmeBool" == "y" || "$readmeBool" == "Y" ]]; then
+        touch README.md
+        git add README.md
+    fi
 
-if [[ "$readmeBool" == "y" || "$readmeBool" == "Y" ]]; then
-    touch README.md
-    git add README.md
+    while [[ "$fileBool" == "y" || "$fileBool" == "Y" ]]; do
+        read -p "Enter filename: " filename
+        touch "$filename"
+        git add "$filename"
+        read -p "Do you want to create any other file for this repo [y/n]: " fileBool
+    done
 fi
 
-read -p "Do you want to add a any other file in this repo [y/n]: " fileBool
-if [[ "$fileBool" == "y" || "$fileBool" == "Y" ]]; then
-    read -p "Enter filename : " filename
-    touch $filename
-    nano $filename
-    git add $file
+read -p "Do you want to add any other file to this repo [y/n]: " eFile
+
+while [[ "$eFile" == "y" || "$eFile" == "Y" ]]; do
+    fileAdded=true
+    read -p "Enter file path: " filepath
+    cp "$filepath" .
+    read -p "Do you want to add any other file to this repo [y/n]: " eFile
+done
+
+if [[ "$fileAdded" == true ]]; then
+    read -p "Enter the commit message: " commitMSG
+    git add .
+    git commit -m "$commitMSG"
+    git remote add origin "https://github.com/$ghUsername/$repoName.git"
+    git push -u origin master
 fi
 
-git commit -m "$commitMSG"
-git remote add origin "https://github.com/$ghUsername/$repoName.git"
-git push -u origin master
+echo "Repository created successfully"
